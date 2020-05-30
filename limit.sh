72e717f6312ddfb4c397479968a6ecb1db03ae02
95dewadew <41128556+95dewadew@users.noreply.github.com>
2020-05-30 17:13:14 +0700

    Update limit.sh

--- limit.sh
+++ limit.sh
@@ -1,44 +1,92 @@
 #!/bin/bash
+
+MYIP=$(wget -qO- ipv4.icanhazip.com)
+
+
 PARAM=$1
+
 echo -n > /tmp/pid2
 ps ax|grep dropbear > /tmp/pid
 cat /tmp/pid | grep -i 'dropbear -p' > /tmp/pids
 cat /var/log/secure |  grep -i "Password auth succeeded" > /tmp/sks
 perl -pi -e 's/Password auth succeeded for//g' /tmp/sks
 perl -pi -e 's/dropbear//g' /tmp/sks
+
 cat /tmp/pid | while read line;do
-set -- $line
-p=$1
-var=`cat /tmp/sks | grep -i $1`
-set -- $var
-l=$6
-if [ "$6" != '' ]
-then
-echo "$p $l" | cat - /tmp/pid2 > /tmp/temp && mv /tmp/temp /tmp/pid2
-fi
+	set -- $line
+	p=$1
+	var=`cat /tmp/sks | grep -i $1`
+	set -- $var
+	l=$6
+	if [ "$6" != '' ]; then
+		echo "$p $l" | cat - /tmp/pid2 > /tmp/temp && mv /tmp/temp /tmp/pid2
+	fi
 done
-echo -n > /tmp/user1
-echo -n > /tmp/user2
-echo -n > /tmp/user3
-cat /tmp/pid2 | while read line;do
-set -- $line
-p=$1
-u=$2
-cat /tmp/user1 | grep -i $u > /dev/null
-if [ $? = 1 ];then
-echo $line >> /tmp/user1
-echo $line >> /tmp/user2
-echo $line >> /tmp/user3
-else
-kill $p
-echo "kill $p user $u"
-fi
-done
-rm -f /tmp/pid
-rm -f /tmp/pid2
-rm -f /tmp/pids
-rm -f /tmp/sks
-rm -f /tmp/user1
-rm -f /tmp/user2
-rm -f /tmp/user3
-exit 0
+
+case $PARAM in
+1)
+	echo -n > /tmp/user1
+	cat /tmp/pid2 | while read line;do
+		set -- $line
+		p=$1
+		u=$2
+		cat /tmp/user1 | grep -i $u > /dev/null
+		if [ $? = 1 ]; then
+			echo $line >> /tmp/user1
+		else
+			kill $p
+			echo "kill $p user $u"
+		fi
+	done
+	rm -f /tmp/pid
+	rm -f /tmp/pid2
+	rm -f /tmp/pids
+	rm -f /tmp/sks
+	rm -f /tmp/user1
+  service dropbear restart
+	exit 0
+;;
+2)
+	echo -n > /tmp/user1
+	echo -n > /tmp/user2
+	cat /tmp/pid2 | while read line;do
+		set -- $line
+		p=$1
+		u=$2
+		cat /tmp/user1 | grep -i $u > /dev/null
+		if [ $? = 1 ]; then
+			echo $line >> /tmp/user1
+		else
+			cat /tmp/user2 | grep -i $u > /dev/null
+			if [ $? = 1 ]; then
+				echo $line >> /tmp/user2
+			else
+				kill $p
+				echo "kill $p user $u"
+			fi
+		fi
+	done
+	rm -f /tmp/pid
+	rm -f /tmp/pid2
+	rm -f /tmp/pids
+	rm -f /tmp/sks
+	rm -f /tmp/user1
+	rm -f /tmp/user2
+  service dropbear restart
+	exit 0
+;;
+*)
+	echo " Gunakan perintah user-limit 1 untuk limit 1 login saja"
+	echo " atau user-limit 2  untuk melimit max 2 login"
+	rm -f /tmp/pid
+	rm -f /tmp/pid2
+	rm -f /tmp/pids
+	rm -f /tmp/sks
+	exit 1
+;;
+esac
+
+service dropbear restart
+
+cd ~/
+rm -f /root/IP
