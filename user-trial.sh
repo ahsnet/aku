f1fe73e146a0c3f967e8d2b18ae1803de319cd3f
ahsnet <66269973+ahsnet@users.noreply.github.com>
2020-06-02 03:06:22 +0700

    Update user-trial.sh

--- user-trial.sh
+++ user-trial.sh
@@ -9,9 +9,13 @@ IP=`dig +short myip.opendns.com @resolver1.opendns.com`
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e "Host: $IP" 
-echo -e "Port: 22, 143, 443, 109, 110"
 echo -e "Username: $Login "
 echo -e "Password: $Pass\n"
+echo "SSL/TLS : 443"
+echo "Dropbear: 110, 109"
+echo "Openssh : 22, 143"
+echo "Squid   : 80, 8080, 3128"
+echo "UdpGw   : 7300
 echo -e ""
 echo -e "Akun ini hanya aktif 1 hari"
 echo -e "============================"
